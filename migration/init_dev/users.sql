6537c934d0a5648d6c48f47307880c70e974d27f
Hugo SABOURIN <hsabouri@student.42.fr>
2023-03-28 18:40:11 +0200

    add: databases with some data

---
+++ migration/init_dev/users.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users {
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4 ();
+    name VARCHAR(16)
+};
+
+INSERT INTO users (user_id, name) VALUES ('a1234567-1234-5678-1234-567812345678', 'Alice');
+INSERT INTO users (user_id, name) VALUES ('b1234567-1234-5678-1234-567812345678', 'Bob');
+INSERT INTO users (user_id, name) VALUES ('c1234567-1234-5678-1234-567812345678', 'Charlie');
