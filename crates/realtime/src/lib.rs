2c315b2f0d5c26492a77913aea96e149ef576784
Hugo SABOURIN <hugo.sabourin@externe.bnpparibas.com>
2023-04-06 11:15:31 +0200

    add: NATS simple test

---
+++ crates/realtime/src/lib.rs
@@ -0,0 +1,41 @@
+#[cfg(test)]
+#[tokio::test]
+async fn message_broadcast_test() -> Result<(), Box<dyn std::error::Error>> {
+    use async_nats::ConnectOptions;
+    use futures::StreamExt;
+
+    println!("Testing a simple NATS case: send/recv");
+
+    let sender = tokio::spawn(async move {
+        let sender = ConnectOptions::new()
+            .connect("nats://ruser:password@127.0.0.1:4222")
+            .await
+            .unwrap();
+        println!("SENDER: Connected to NATS: {}", sender.connection_state());
+
+        for _ in 0..10 {
+            let _ = tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            let _ = sender.publish("notification".into(), "ouai".into()).await;
+            println!("SENDER: Sent message");
+        }
+    });
+
+    let receiver_1 = tokio::spawn(async move {
+        let receiver = ConnectOptions::new()
+            .connect("nats://ruser:password@127.0.0.1:4222")
+            .await
+            .unwrap();
+        println!(
+            "RECEIVER_1: Connected to NATS: {}",
+            receiver.connection_state()
+        );
+
+        let mut sub = receiver.subscribe("notification".into()).await.unwrap();
+
+        println!("RECEIVER_1: message: \"{:#?}\"", sub.next().await);
+    });
+
+    let _ = tokio::join!(receiver_1, sender);
+
+    Ok(())
+}
