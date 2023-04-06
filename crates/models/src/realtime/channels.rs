420b94a9aee263336dd52414cdf7e03367c54c9d
Hugo SABOURIN <hugo.sabourin@externe.bnpparibas.com>
2023-04-06 17:56:08 +0200

    add: unit stream wrapping NATS output

--- crates/models/src/realtime/channels.rs
+++ crates/models/src/realtime/channels.rs
@@ -1,2 +1,4 @@
-pub static CHANNEL_USER_MESSAGE: &'static str = "user_message";
+pub static CHANNEL_MESSAGE: &'static str = "message";
 pub static CHANNEL_FRIENDSHIP: &'static str = "friendship";
+pub static CHANNEL_MESSAGE_SEEN: &'static str = "seen_message";
+pub static CHANNEL_MESSAGE_UNSEEN: &'static str = "unseen_message";
--- crates/models/src/realtime/mod.rs
+++ crates/models/src/realtime/mod.rs
@@ -3,7 +3,7 @@
 use std::{collections::HashSet, pin::Pin, task::Poll};
 
 use anyhow::Error;
-use async_nats::{Client, Subscriber};
+use async_nats::Client;
 use chrono::NaiveDateTime;
 use futures::{stream::StreamExt, Stream};
 use prost::Message as ProstMessage;
@@ -14,7 +14,7 @@ mod channels;
 use channels::*;
 
 use crate::{
-    messages::Message,
+    messages::{Message, MessageRef},
     users::{UserRef, Userlike},
 };
 
@@ -31,23 +31,145 @@ fn parse_proto_message(payload: prost::bytes::Bytes) -> Result<Message, Error> {
     Ok(message)
 }
 
-fn parse_proto_friendship_notification(
-    payload: prost::bytes::Bytes,
-) -> Result<(UserRef, UserRef), Error> {
-    let friendship = proto::FriendshipNotification::decode_length_delimited(payload)?;
+fn parse_proto_friendship(payload: prost::bytes::Bytes) -> Result<(UserRef, UserRef), Error> {
+    let friendship = proto::Friendship::decode_length_delimited(payload)?;
 
     let user = UserRef(Uuid::try_parse(friendship.user.as_str())?);
-
     let friend = UserRef(Uuid::try_parse(friendship.friend.as_str())?);
 
     Ok((user, friend))
+}
+
+fn parse_proto_message_tag_request(
+    payload: prost::bytes::Bytes,
+) -> Result<(UserRef, MessageRef), Error> {
+    let tag = proto::MessageTagRequest::decode_length_delimited(payload)?;
+
+    let user = UserRef(Uuid::try_parse(tag.user_id.as_str())?);
+    let message = MessageRef(Uuid::try_parse(tag.message_id.as_str())?);
+
+    Ok((user, message))
+}
+
+/// Stream of all new messages from all users.
+pub struct NewMessages {
+    inner: Pin<Box<dyn Stream<Item = Result<Message, Error>>>>,
+}
+
+impl NewMessages {
+    pub async fn new(client: Client) -> Result<Self, Error> {
+        let subscription = client.subscribe(CHANNEL_MESSAGE.into()).await?;
+
+        let inner = subscription.map(|proto_message| parse_proto_message(proto_message.payload));
+
+        Ok(Self {
+            inner: Box::pin(inner),
+        })
+    }
+}
+
+impl Stream for NewMessages {
+    type Item = Result<Message, Error>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        self.inner.poll_next_unpin(cx)
+    }
+}
+
+/// Stream of all new friendships for all users.
+pub struct NewFriendships {
+    inner: Pin<Box<dyn Stream<Item = Result<(UserRef, UserRef), Error>>>>,
+}
+
+impl NewFriendships {
+    pub async fn new(client: Client) -> Result<Self, Error> {
+        let subscription = client.subscribe(CHANNEL_FRIENDSHIP.into()).await?;
+
+        let inner = subscription.map(|proto_message| parse_proto_friendship(proto_message.payload));
+
+        Ok(Self {
+            inner: Box::pin(inner),
+        })
+    }
+}
+
+impl Stream for NewFriendships {
+    type Item = Result<(UserRef, UserRef), Error>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        self.inner.poll_next_unpin(cx)
+    }
+}
+
+/// Stream of all new friendships for all users.
+pub struct SeenMessages {
+    inner: Pin<Box<dyn Stream<Item = Result<(UserRef, MessageRef), Error>>>>,
+}
+
+impl SeenMessages {
+    pub async fn new(client: Client) -> Result<Self, Error> {
+        let subscription = client.subscribe(CHANNEL_MESSAGE_SEEN.into()).await?;
+
+        let inner = subscription
+            .map(|proto_message| parse_proto_message_tag_request(proto_message.payload));
+
+        Ok(Self {
+            inner: Box::pin(inner),
+        })
+    }
+}
+
+impl Stream for SeenMessages {
+    type Item = Result<(UserRef, MessageRef), Error>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        self.inner.poll_next_unpin(cx)
+    }
+}
+
+/// Stream of all new friendships for all users.
+pub struct UnseenMessages {
+    inner: Pin<Box<dyn Stream<Item = Result<(UserRef, MessageRef), Error>>>>,
+}
+
+impl UnseenMessages {
+    pub async fn new(client: Client) -> Result<Self, Error> {
+        let subscription = client.subscribe(CHANNEL_MESSAGE_UNSEEN.into()).await?;
+
+        let inner = subscription
+            .map(|proto_message| parse_proto_message_tag_request(proto_message.payload));
+
+        Ok(Self {
+            inner: Box::pin(inner),
+        })
+    }
+}
+
+impl Stream for UnseenMessages {
+    type Item = Result<(UserRef, MessageRef), Error>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        self.inner.poll_next_unpin(cx)
+    }
 }
 
 /// Takes a stream `T` of `I::Userlike` and outputs a stream of the newly posted messages from these users.
 ///
 /// If stream `T` is closed/finished, output stream will continue with newly posted message of all users returned by stream
 /// `T` before it closed.
-/// 
+///
 /// Demonstrates a dynamic filter stream.
 pub struct UsersNewMessages<T, I>
 where
@@ -55,7 +177,7 @@ where
     I: Userlike,
 {
     users_stream: Option<T>,
-    subscription: Subscriber,
+    subscription: NewMessages,
     users: HashSet<Uuid>,
 }
 
@@ -67,7 +189,7 @@ where
     pub async fn new(users: T, client: Client) -> Result<Self, Error> {
         Ok(Self {
             users_stream: Some(users),
-            subscription: client.subscribe(CHANNEL_USER_MESSAGE.into()).await?,
+            subscription: NewMessages::new(client).await?,
             users: HashSet::new(),
         })
     }
@@ -108,8 +230,8 @@ where
         // Get potential new message from subscribtion
         match self.subscription.poll_next_unpin(cx) {
             Poll::Ready(output) => match output {
-                Some(nats_message) => {
-                    match parse_proto_message(nats_message.payload) {
+                Some(message) => {
+                    match message {
                         Ok(message) => {
                             // Filtering with users in the list
                             if self.users.contains(&message.user_id) {
@@ -118,7 +240,7 @@ where
                                 Poll::Pending
                             }
                         }
-                        Err(e) => Poll::Ready(Some(Err(Error::from(e)))),
+                        err => Poll::Ready(Some(err)),
                     }
                 }
                 None => Poll::Ready(None),
@@ -137,13 +259,13 @@ pub struct UserNewFriendships {
 
 impl UserNewFriendships {
     pub async fn new(user_id: Uuid, client: Client) -> Result<Self, Error> {
-        let subscription = client.subscribe(CHANNEL_FRIENDSHIP.into()).await?;
+        let friendships = NewFriendships::new(client).await?;
 
-        let inner = subscription.filter_map(move |proto_message| async move {
-            match parse_proto_friendship_notification(proto_message.payload) {
+        let inner = friendships.filter_map(move |friendship| async move {
+            match friendship {
                 Ok((user, friend)) if user.get_uuid() == user_id => Some(Ok(friend)),
                 Ok(_) => None,
-                Err(e) => Some(Err(Error::from(e))),
+                Err(e) => Some(Err(e)),
             }
         });
 
